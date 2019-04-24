7dcd67d288ed76f186c5b9cb542c8442ffe2b6ef
Rick Rosetti <rick.rosetti@gmail.com>
2019-04-24 13:45:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,13 @@
 # Add  code here!
 def prime(int)
   test_range = (2...int).to_a
+case test_range
+when 2
+  return true
+when <= 0
+  return false
+else  
   test_range.none? {|i| int % i == 0}
 end
+end
+
